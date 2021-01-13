2a26d3bea3ae53e59fa5d6ac539ddaef8dbb830c
Aleix <aleix2077@gmail.com>
2021-01-13 16:14:32 +0100

    CERCLE CORRECTE ACABAT

--- src/framework/image.cpp
+++ src/framework/image.cpp
@@ -281,10 +281,22 @@ void Image::drawCircle(int start_x, int start_y, int r, Color color, bool fill) 
 		{
 			for (int y = start_y-r; y < ((start_y - r) + 2 * r); ++y)
 			{
-				if (((x- start_x)*(x- start_x) + (y- start_y)*(y- start_y))-(r*r) <= 0 && ((x - start_x) * (x - start_x) + (y - start_y) * (y - start_y)) - (r * r) >= -2*r){
+				if (((x- start_x)*(x- start_x) + (y- start_y)*(y- start_y))-(r*r) <= 0 ){
 					this->setPixel(x, y, color);
 				}
 			}
 		}
 	}	
+	else
+	{
+		for (int x = start_x - r; x < ((start_x - r) + 2 * r); ++x)
+		{
+			for (int y = start_y - r; y < ((start_y - r) + 2 * r); ++y)
+			{
+				if (((x - start_x) * (x - start_x) + (y - start_y) * (y - start_y)) - (r * r) <= 0 && ((x - start_x) * (x - start_x) + (y - start_y) * (y - start_y)) - (r * r) >= -2 * r) {
+					this->setPixel(x, y, color);
+				}
+			}
+		}
+	}
 }
