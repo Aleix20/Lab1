883489fed36d24766601cc21cd200c5caa444dbd
Albert <albert.gubau01@estudiant.upf.edu>
2021-01-18 13:28:01 +0100

    Seleccionador de tasques a executar amb la hotbar de números

--- src/framework/application.cpp
+++ src/framework/application.cpp
@@ -3,6 +3,7 @@
 #include "image.h"
 
 Image img;
+int var;
 
 Application::Application(const char* caption, int width, int height)
 {
@@ -28,7 +29,7 @@ void Application::init(void)
 
 	//here add your init stuff
 	
-	img.loadTGA("../../res/image.tga");
+	img.loadTGA("../res/image.tga");
 	img.scale(window_width, window_height);
 }
 
@@ -36,33 +37,90 @@ void Application::init(void)
 void Application::render( Image& framebuffer )
 {
 	//clear framebuffer if we want to start from scratch
-	framebuffer.fill(Color::BLACK);
+	//framebuffer.fill(Color::BLACK);
 	
-	//filling the framebuffer with the image
-	/*for (unsigned int x = 0; x < framebuffer.width; x++) {
-		for (unsigned int y = 0; y < framebuffer.height; y++) {
-			framebuffer.setPixel(x, y, img.getPixelSafe(x, y));
-		}
-	}*/
+	if (var == 1) {
+		//Task 1: Simple figures
+		framebuffer.fill(Color::BLACK);
+		
+		framebuffer.drawRectangle(330, 450, 50, 70, Color(255, 0, 0), true);
+		framebuffer.drawRectangle(420, 450, 50, 70, Color(255, 0, 0), false);
+		
+		framebuffer.drawCircle(345, 375, 45, Color(0, 255, 0), true);
+		framebuffer.drawCircle(455, 375, 45, Color(0, 255, 0), false);
 
-	//Task 1: Simple figures
-	framebuffer.drawRectangle(50, 50, 50, 70, Color(20, 20, 20), true);
-	//framebuffer.drawCircle(400, 300, 300, Color(255, 0, 0), false);
-	framebuffer.drawLine(400, 200, 400, 100, Color::BLUE);
-	
+		framebuffer.drawLine(400, 200, 400, 300, Color(0,0,255));
+		framebuffer.drawLine(400, 200, 500, 300, Color(0, 0, 255));
+		
+		framebuffer.drawLine(400, 200, 500, 200, Color(0, 0, 255));
+		framebuffer.drawLine(400, 200, 500, 100, Color(0, 0, 255));
+		
+		framebuffer.drawLine(400, 200, 400, 100, Color(0, 0, 255));
+		framebuffer.drawLine(400, 200, 300, 100, Color(0, 0, 255));
+		
+		framebuffer.drawLine(400, 200, 300, 200, Color(0, 0, 255));
+		framebuffer.drawLine(400, 200, 300, 300, Color(0, 0, 255));
+
+	}
+
 	//Task 2: Gradients
-	//framebuffer.drawGradientH(Color::BLUE, Color::RED);
-	//framebuffer.drawRadialGradient(Color::BLACK, Color::WHITE);
+	if (var == 2) {
+		framebuffer.fill(Color::BLACK);
+		framebuffer.drawGradientH(Color::BLUE, Color::RED);
+	}
 
+	if (var == 3) {
+		framebuffer.fill(Color::BLACK);
+		//framebuffer.drawRadialGradient(Color::BLACK, Color::WHITE);
+	}
+	
 	//Task 3: Effects
-	//framebuffer.greyImg(img);
-	//framebuffer.invertImg(img);
+	if (var == 4) {
+		//filling the framebuffer with the image
+		for (unsigned int x = 0; x < framebuffer.width; x++) {
+			for (unsigned int y = 0; y < framebuffer.height; y++) {
+				framebuffer.setPixel(x, y, img.getPixelSafe(x, y));
+			}
+		}
+	}
+	
+	if (var == 5) {
+		//filling the framebuffer with the image
+		for (unsigned int x = 0; x < framebuffer.width; x++) {
+			for (unsigned int y = 0; y < framebuffer.height; y++) {
+				framebuffer.setPixel(x, y, img.getPixelSafe(x, y));
+			}
+		}
+
+		framebuffer.greyImg(img);
+	}
+	
+	if (var == 6) {
+		//filling the framebuffer with the image
+		for (unsigned int x = 0; x < framebuffer.width; x++) {
+			for (unsigned int y = 0; y < framebuffer.height; y++) {
+				framebuffer.setPixel(x, y, img.getPixelSafe(x, y));
+			}
+		}
+
+		framebuffer.invertImg(img);
+	}
 	
 	//Task 4: Warping
-	//framebuffer.rotateImg(img, 45);
-	//framebuffer.scaleImg(img,10.0);
+	if (var == 7) {
+		framebuffer.fill(Color::BLACK);
+		framebuffer.rotateImg(img, 45);
+	}
 
-	
+	if (var == 8) {
+		//filling the framebuffer with the image
+		for (unsigned int x = 0; x < framebuffer.width; x++) {
+			for (unsigned int y = 0; y < framebuffer.height; y++) {
+				framebuffer.setPixel(x, y, img.getPixelSafe(x, y));
+			}
+		}
+		framebuffer.scaleImg(img, 10.0);
+	}
 }
 
 //called after render
@@ -78,14 +136,46 @@ void Application::update(double seconds_elapsed)
 }
 
 //keyboard press event 
-void Application::onKeyDown( SDL_KeyboardEvent event )
+void Application::onKeyDown(SDL_KeyboardEvent event)
 {
 	//to see all the keycodes: https://wiki.libsdl.org/SDL_Keycode
-	switch(event.keysym.scancode)
+	switch (event.keysym.scancode)
 	{
-		case SDL_SCANCODE_ESCAPE:
-			exit(0); 
-			break; //ESC key, kill the app
+	case SDL_SCANCODE_ESCAPE:
+		exit(0);
+		break; //ESC key, kill the app	
+
+	case SDL_SCANCODE_1:
+		var = 1;
+		break;
+
+	case SDL_SCANCODE_2:
+		var = 2;
+		break;
+
+	case SDL_SCANCODE_3:
+		var = 3;
+		break;
+
+	case SDL_SCANCODE_4:
+		var = 4;
+		break;
+
+	case SDL_SCANCODE_5:
+		var = 5;
+		break;
+
+	case SDL_SCANCODE_6:
+		var = 6;
+		break;
+
+	case SDL_SCANCODE_7:
+		var = 7;
+		break;
+
+	case SDL_SCANCODE_8:
+		var = 8;
+		break;
 	}
 }
 
@@ -101,6 +191,7 @@ void Application::onMouseButtonDown( SDL_MouseButtonEvent event )
 	if (event.button == SDL_BUTTON_LEFT) //left mouse pressed
 	{
 		//if you read mouse position from the event, careful, Y is reversed, use mouse_position instead
+
 	}
 }
 
